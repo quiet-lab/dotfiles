e3b1f4b841aeab6cec3353df8cff2898c74f5bf5
Пинчук Александр Владимирович <a.pinchuk@axata.by>
2026-08-22 12:38:26 +0300

    Fix yuck lsp

--- dot_config/nvim/lua/plugins/lisp.lua
+++ dot_config/nvim/lua/plugins/lisp.lua
@@ -15,28 +15,28 @@ vim.filetype.add({
   },
 })
 
--- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
---   pattern = { "*.yuck" },
---   callback = function(event)
---     print(string.format("starting yuck;s for %s", vim.inspect(event)))
---     vim.lsp.start({
---       name = "YuckLs",
---       cmd = { "dotnet", "/home/gitrepos/yuckls/YuckLS/dist/YuckLS.dll" }, --this must be where you cloned this repo to.
---       --cmd = {"yuckls"} -- if installed from aur
---       root_dir = vim.fn.getcwd(),
---     })
---   end,
--- })
-
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-  pattern = "*.yuck",
-  callback = function()
+  pattern = { "*.yuck" },
+  callback = function(event)
+    print(string.format("starting yuck;s for %s", vim.inspect(event)))
     vim.lsp.start({
-      name = "yuck-lsp",
-      cmd = { "yuckls" }, -- Replace with your local executable name
-      root_dir = vim.fs.dirname(vim.fs.find({ "eww.yuck" }, { upward = true })[1]),
+      name = "YuckLs",
+      -- cmd = { "dotnet", "/home/gitrepos/yuckls/YuckLS/dist/YuckLS.dll" }, --this must be where you cloned this repo to.
+      cmd = { "yuckls" }, -- if installed from aur
+      root_dir = vim.fn.getcwd(),
     })
   end,
 })
+
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+--   pattern = "*.yuck",
+--   callback = function()
+--     vim.lsp.start({
+--       name = "yuck-lsp",
+--       cmd = { "yuckls" }, -- Replace with your local executable name
+--       root_dir = vim.fs.dirname(vim.fs.find({ "eww.yuck" }, { upward = true })[1]),
+--     })
+--   end,
+-- })
 
 return { { "eraserhd/parinfer-rust", build = "cargo build --release" } }
