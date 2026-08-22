45c6bb110271bba17c8b48bab9649e4123feeb87
Пинчук Александр Владимирович <a.pinchuk@axata.by>
2026-08-22 12:43:25 +0300

    Fix yuck lsp

--- dot_config/nvim/lua/plugins/lisp.lua
+++ dot_config/nvim/lua/plugins/lisp.lua
@@ -18,7 +18,7 @@ vim.filetype.add({
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.yuck" },
   callback = function(event)
-    print(string.format("starting yuck;s for %s", vim.inspect(event)))
+    -- print(string.format("starting yuck;s for %s", vim.inspect(event)))
     vim.lsp.start({
       name = "YuckLs",
       -- cmd = { "dotnet", "/home/gitrepos/yuckls/YuckLS/dist/YuckLS.dll" }, --this must be where you cloned this repo to.
